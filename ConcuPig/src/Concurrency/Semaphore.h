35ff7df424ded97c5d4ce86c8ff1c80335b9fa5e
sebastianr213@gmail.com <sebastianr213@gmail.com@55f93c61-a691-bee4-d3df-75a0d1503373>
2013-04-27 18:06:51 +0000

    # Semaphore class

---
+++ ConcuPig/src/Concurrency/Semaphore.h
@@ -0,0 +1,27 @@
+#ifndef SEMPAHORE_H_
+#define SEMPAHORE_H_
+
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/types.h>
+#include <string>
+
+class Semaphore {
+
+private:
+	int id;
+	int initialValue;
+
+	int initialize ();
+
+public:
+	Semaphore (std::string& name, int initialValue);
+	virtual ~Semaphore();
+
+	int wait ();
+	int signal ();
+	void deleteSemaphore ();
+};
+
+
+#endif /* SEMPAHORE_H_ */
