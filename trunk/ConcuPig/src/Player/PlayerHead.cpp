2be8157c4c77cdaad4e348c7e1fd0c12bf9e2e37
servetto.matias@gmail.com <servetto.matias@gmail.com@55f93c61-a691-bee4-d3df-75a0d1503373>
2013-04-27 23:46:20 +0000

    Player Head with tested methods for pick a card to sent and see if one won

--- trunk/ConcuPig/src/Player/PlayerHead.cpp
+++ trunk/ConcuPig/src/Player/PlayerHead.cpp
@@ -1,16 +1,12 @@
-/*
- * PlayerHead.cpp
- *
- *  Created on: 27/04/2013
- *      Author: matias
- */
-
 #include "PlayerHead.h"
 #include "../Services/NamingService.h"
 #include "../Constants/SemaphoreNames.h"
 #include "../Constants/SharedMemoryNames.h"
+#include "../Constants/SignalNumbers.h"
+#include <map>
 
-PlayerHead::PlayerHead(int playerNumber, int leftPlayerNumber, int rightPlayerNumber) :
+PlayerHead::PlayerHead(pid_t fatherId, int playerNumber, int leftPlayerNumber, int rightPlayerNumber) :
+	fatherId(fatherId),
 	number(playerNumber),
 	leftPlayerNumber(leftPlayerNumber),
 	rightPlayerNumber(rightPlayerNumber),
@@ -46,11 +42,45 @@ void PlayerHead::playRound()
 	}
 }
 
+void PlayerHead::takeCard( Card card )
+{
+	this->hand.push_back(card);
+}
+
 Card PlayerHead::retrieveCardToSend()
 {
-	// TODO implment a stategy to decide the position o the card to grab
+	// TODO implement a stategy to decide the position o the card to grab
+	std::map<char, int> dictionary;
+	for(unsigned int indexCard = 0 ; indexCard < this->hand.size() ; indexCard++)
+	{
+		Card card = this->hand[indexCard];
+		std::map<char, int>::iterator it = dictionary.find(card.getSymbol());
+		int ocurrencies = 1;
+		if( dictionary.end() != it )
+		{
+			ocurrencies += it->second;
+		}
+		dictionary[card.getSymbol()] = ocurrencies;
+	}
+	std::map<char, int>::iterator minItMap = dictionary.begin();
+	std::map<char, int>::iterator itMap = dictionary.begin();
+	while( dictionary.end() != itMap )
+	{
+		if( minItMap->second > itMap->second )
+		{
+			minItMap = itMap;
+		}
+		itMap++;
+	}
+	bool notFountSymbol = true;
 	int position = 0;
-	//
+	while( notFountSymbol )
+	{
+		if( this->hand[position].getSymbol() == minItMap->first )
+			notFountSymbol = false;
+		else
+			position++;
+	}
 
 	Card returnedCard = this->hand[position];
 	std::vector<Card>::iterator it = this->hand.begin();
@@ -61,20 +91,20 @@ Card PlayerHead::retrieveCardToSend()
 
 void PlayerHead::informCardHasBeenSelected()
 {
-	// TODO implement
+	kill(this->fatherId , SignalNumber::CardSelected);
 }
 
 void PlayerHead::informMyHandIsOnTheTable()
 {
-	// TODO implement
+	kill(this->fatherId , SignalNumber::PlayerWon);
 }
 
 bool PlayerHead::isWinningHand()
 {
 	return
-			(this->hand[0].getNumber() == this->hand[1].getNumber()) &&
-			(this->hand[1].getNumber() == this->hand[2].getNumber()) &&
-			(this->hand[2].getNumber() == this->hand[3].getNumber());
+			(this->hand[0].getSymbol() == this->hand[1].getSymbol()) &&
+			(this->hand[1].getSymbol() == this->hand[2].getSymbol()) &&
+			(this->hand[2].getSymbol() == this->hand[3].getSymbol());
 }
 
 PlayerHead::~PlayerHead() { }
