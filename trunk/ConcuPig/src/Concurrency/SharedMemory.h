2e2799297e699ea4dd5d8417e54bb04a0e176205
damian.schenkelman@gmail.com <damian.schenkelman@gmail.com@55f93c61-a691-bee4-d3df-75a0d1503373>
2013-04-27 17:34:21 +0000

    Initial commit with shared memory

---
+++ trunk/ConcuPig/src/Concurrency/SharedMemory.h
@@ -0,0 +1,101 @@
+/*
+ * SharedMemory.h
+ *
+ *  Created on: Apr 27, 2013
+ *      Author: dschenkelman
+ */
+
+#ifndef SHAREDMEMORY_H_
+#define SHAREDMEMORY_H_
+
+#define SHM_OK 0
+#define	FTOK_ERROR -1
+#define SHMGET_ERROR -2
+#define	SHMAT_ERROR -3
+
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <string>
+
+template <class T> class SharedMemory {
+
+private:
+
+	int 	shmId;
+	T*		dataPointer;
+
+	int attachedProcessesCount ();
+
+public:
+
+	SharedMemory ();
+	~SharedMemory ();
+	int create (std::string& fileName,char character );
+	void release ();
+	void setValue ( T value );
+	T getValue ();
+
+};
+
+template <class T> SharedMemory<T> :: SharedMemory () {
+	this->shmId = 0;
+	this->dataPointer = NULL;
+}
+
+template <class T> SharedMemory<T> :: ~SharedMemory () {
+}
+
+template <class T> int SharedMemory<T> :: create ( std::string& fileName,char letra ) {
+	// key generation
+	key_t key = ftok(fileName.c_str(), letra);
+	if (-1 == key)
+		return FTOK_ERROR;
+	else {
+		// create shared memory
+		this->shmId = shmget(key, sizeof(T), 0644|IPC_CREAT);
+
+		if ( this->shmId == -1 )
+			return SHMGET_ERROR;
+		else {
+			// attach memory block to process address space
+			void* temp = shmat(this->shmId, NULL, 0);
+
+			if ( temp == (void *) -1 ) {
+				return SHMAT_ERROR;
+			} else {
+				this->dataPointer = (T *) temp;
+				return SHM_OK;
+			}
+		}
+	}
+}
+
+
+template <class T> void SharedMemory<T> :: release () {
+	// detach memory block
+	shmdt ((void *) this->dataPointer );
+
+	int attachedProcesses = this->attachedProcessesCount();
+
+	if (attachedProcesses == 0) {
+		shmctl ( this->shmId,IPC_RMID,NULL );
+	}
+}
+
+template <class T> void SharedMemory<T> :: setValue ( T value ) {
+	*(this->dataPointer) = value;
+}
+
+template <class T> T SharedMemory<T> :: getValue () {
+	return (*(this->dataPointer));
+}
+
+template <class T> int SharedMemory<T> :: attachedProcessesCount () {
+	shmid_ds state;
+	shmctl (this->shmId, IPC_STAT, &state);
+	return state.shm_nattch;
+}
+
+#endif /* SHAREDMEMORY_H_ */
+
