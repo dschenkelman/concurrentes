9a1e15de4c91d8f1e8d6caf3b3263e8bea637154
sebastianr213@gmail.com <sebastianr213@gmail.com@55f93c61-a691-bee4-d3df-75a0d1503373>
2013-04-27 21:26:15 +0000

    # SharedCard skeleton

--- ConcuPig/src/Model/SharedCard.h
+++ ConcuPig/src/Model/SharedCard.h
@@ -2,16 +2,22 @@
 #define SHAREDCARD_H_
 #include "../Concurrency/SharedMemory.h"
 #include "../Services/NamingService.h"
+#include "Card.h"
 
 class SharedCard {
 
 private:
-
+	SharedMemory<Card> sharedCardMemory;
+	int sharedMemoryState;
 
 public:
 
 	SharedCard (std::string& cardType, int playerNumber);
 	virtual ~SharedCard();
+
+	void setCard(Card card);
+
+	Card getCard();
 
 };
 
