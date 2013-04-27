3f1f7706a04882bb7c1ae2086f7d70b7770553c9
sebastianr213@gmail.com <sebastianr213@gmail.com@55f93c61-a691-bee4-d3df-75a0d1503373>
2013-04-27 18:06:51 +0000

    # Semaphore class

---
+++ trunk/ConcuPig/src/Concurrency/Semaphore.cpp
@@ -0,0 +1,58 @@
+#include "Semaphore.h"
+
+using namespace std;
+
+Semaphore :: Semaphore ( string &name,int initialValue ) {
+
+	this->initialValue = initialValue;
+
+	key_t key = ftok ( name.c_str(),'a' );
+	this->id = semget ( key,1,0666 | IPC_CREAT );
+
+	this->initialize ();
+}
+
+Semaphore::~Semaphore() {
+}
+
+int Semaphore :: initialize () {
+
+	union semnum {
+		int val;
+		struct semid_ds* buf;
+		ushort* array;
+	};
+
+	semnum init;
+	init.val = this->initialValue;
+	int result = semctl ( this->id,0,SETVAL,init );
+	return result;
+}
+
+int Semaphore :: wait () {
+
+	struct sembuf operation;
+
+	operation.sem_num = 0;	// Semaphore number
+	operation.sem_op  = -1;	// Decrease by one the semaphore
+	operation.sem_flg = SEM_UNDO;
+
+	int result = semop ( this->id,&operation,1 );
+	return result;
+}
+
+int Semaphore :: signal () {
+
+	struct sembuf operation;
+
+	operation.sem_num = 0;	// Semaphore number
+	operation.sem_op  = 1;	// Increase by one the semaphore
+	operation.sem_flg = SEM_UNDO;
+
+	int result = semop ( this->id,&operation,1 );
+	return result;
+}
+
+void Semaphore :: deleteSemaphore () {
+	semctl ( this->id,0,IPC_RMID );
+}
