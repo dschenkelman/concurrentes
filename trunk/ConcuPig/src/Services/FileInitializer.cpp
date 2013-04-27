b97ccccfc60c051142499fa344be7dbe1e016895
damian.schenkelman@gmail.com <damian.schenkelman@gmail.com@55f93c61-a691-bee4-d3df-75a0d1503373>
2013-04-27 21:06:36 +0000

    Fix para seba

--- trunk/ConcuPig/src/Services/FileInitializer.cpp
+++ trunk/ConcuPig/src/Services/FileInitializer.cpp
@@ -7,20 +7,24 @@
 
 #include "FileInitializer.h"
 #include "NamingService.h"
+#include "../Constants/SharedMemoryNames.h"
+
+#include <string>
+
+using namespace std;
 
 FileInitializer::FileInitializer(int players) : players(players) {
 }
 
 void FileInitializer::Initialize(){
-	this->InitializeFifos();
+	this->InitializeSharedMemories();
 }
 
-void FileInitializer::InitializeFifos(){
-	for(int i = 0; i < this->players; i++){
-		NamingService::getCardPassingFifoKey(i, (i + 1) % this->players);
+void FileInitializer::InitializeSharedMemories(){
+	for (int i = 0; i < this->players; i++){
+		string receiveCardName = NamingService::getSharedCardFileName(SharedMemoryNames::CardToReceive, i);
+		string sendCardName = NamingService::getSharedCardFileName(SharedMemoryNames::CardToSend, i);
 	}
-
-
 }
 
 FileInitializer::~FileInitializer() {
