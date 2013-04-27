465d522dcee439439c2c6608e3104199151f8d12
servetto.matias@gmail.com <servetto.matias@gmail.com@55f93c61-a691-bee4-d3df-75a0d1503373>
2013-04-27 18:33:08 +0000

    Deleting debug folder to ignore

--- ConcuPig/Debug/src/Concurrency/Fifo.d
+++
@@ -1,4 +0,0 @@
-src/Concurrency/Fifo.d: ../src/Concurrency/Fifo.cpp \
- ../src/Concurrency/Fifo.h
-
-../src/Concurrency/Fifo.h:
--- ConcuPig/Debug/src/Concurrency/SignalHandler.d
+++
@@ -1,6 +0,0 @@
-src/Concurrency/SignalHandler.d: ../src/Concurrency/SignalHandler.cpp \
- ../src/Concurrency/SignalHandler.h ../src/Concurrency/IEventHandler.h
-
-../src/Concurrency/SignalHandler.h:
-
-../src/Concurrency/IEventHandler.h:
