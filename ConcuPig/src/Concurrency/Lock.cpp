07386f05e50ee0757e9f980762f43408c671be3c
servetto.matias@gmail.com <servetto.matias@gmail.com@55f93c61-a691-bee4-d3df-75a0d1503373>
2013-04-27 18:30:44 +0000

    private acquire and release, using them at constructor and destructor.

--- ConcuPig/src/Concurrency/Lock.cpp
+++ ConcuPig/src/Concurrency/Lock.cpp
@@ -1,6 +1,6 @@
 #include "Lock.h"
 
-Lock :: Lock ( std::string name ) {
+Lock :: Lock ( std::string& name ) {
 
 	this->name = name;
 
@@ -11,23 +11,23 @@ Lock :: Lock ( std::string name ) {
 	this->fl.l_pid = getpid ();
 	this->fd = open ( this->name.c_str(),O_CREAT|O_WRONLY,0777 );
 
-	getLock();
+	acquire();
 }
 
 Lock :: ~Lock () {
 	close ( this->fd );
-	freeLock();
+	release();
 }
 
 //Private methods
 
-int Lock :: getLock () {
+int Lock :: acquire () {
 	this->fl.l_type = F_WRLCK;
 	int result = fcntl ( this->fd,F_SETLKW,&(this->fl) );
 	return result;
 }
 
-int Lock :: freeLock () {
+int Lock :: release () {
 	this->fl.l_type = F_UNLCK;
 	int result = fcntl ( this->fd,F_SETLK,&(this->fl) );
 	return result;
