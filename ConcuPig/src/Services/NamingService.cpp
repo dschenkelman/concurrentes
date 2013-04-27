9ebf269a85f5840d52adf0c6b91477333722f4e4
damian.schenkelman@gmail.com <damian.schenkelman@gmail.com@55f93c61-a691-bee4-d3df-75a0d1503373>
2013-04-27 19:09:30 +0000

    Implemented naming service

--- ConcuPig/src/Services/NamingService.cpp
+++ ConcuPig/src/Services/NamingService.cpp
@@ -6,3 +6,33 @@
  */
 
 #include "NamingService.h"
+#include "../Constants/FifoNames.h"
+#include "../Constants/SharedMemoryNames.h"
+#include <string>
+
+using namespace std;
+
+static key_t NamingService::getSemaphoreName(const std::string& kind, int playerNumber){
+	return ftok(kind.c_str(), playerNumber);
+}
+
+static key_t NamingService::getCardPassingFifo(int originPlayer, int targetPlayer){
+	string fileName = FifoNames::CardPassing << "_" + originPlayer << "_" << targetPlayer;
+	return ftok(fileName.c_str(), 'a');
+}
+
+static key_t NamingService::getDealingFifo(int player){
+	return ftok(FifoNames::Dealing.c_str(), player);
+}
+
+static key_t NamingService::getHandDownFifo(){
+	return ftok(FifoNames::HandDown.c_str(), 'a');
+}
+
+static key_t NamingService::getSharedCard(const std::string& cardType, int playerNumber){
+	return ftok(cardType.c_str(), playerNumber);
+}
+
+static key_t NamingService::getScorboard(){
+	return ftok(SharedMemoryNames::Scoreboard.c_str(), 'a');
+}
