91a734b1735431f5caa9b8d14c4e40a2918766a3
servetto.matias@gmail.com <servetto.matias@gmail.com@55f93c61-a691-bee4-d3df-75a0d1503373>
2013-06-20 18:46:53 +0000

    server

--- trunk/DBMS/src/clientMain.cpp
+++ trunk/DBMS/src/clientMain.cpp
@@ -1,4 +1,4 @@
-//#ifdef MAIN_CLIENT
+#ifdef MAIN_CLIENT
 
 #include <iostream>
 #include <stdlib.h>
@@ -143,4 +143,4 @@ void printMessageResponse(int requestAction, list<struct messageResponse>* vMess
 
 }
 
-//#endif
+#endif
