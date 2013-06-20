c115f44fc3d4e88f51eb5d1f642d141ef864ca08
servetto.matias@gmail.com <servetto.matias@gmail.com@55f93c61-a691-bee4-d3df-75a0d1503373>
2013-06-20 14:32:05 +0000

    server main

---
+++ trunk/DBMS/src/serverMain.cpp
@@ -0,0 +1,18 @@
+/*
+ * serverMain.cpp
+ *
+ *  Created on: 20/06/2013
+ *      Author: matias
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	std::cout << "hello";
+	return 0;
+}
