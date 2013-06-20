fb1adf794df148d7c59fab6196352c839b0289a1
sebastianr213@gmail.com <sebastianr213@gmail.com@55f93c61-a691-bee4-d3df-75a0d1503373>
2013-06-20 14:45:07 +0000

    #MessageQueue structure added #Messages defined #Messages actions defined

---
+++ trunk/DBMS/src/Messages/MessageActions.h
@@ -0,0 +1,24 @@
+/*
+ * MessageActions.h
+ *
+ *  Created on: Jun 20, 2013
+ *      Author: sebas
+ */
+
+#ifndef MESSAGEACTIONS_H_
+#define MESSAGEACTIONS_H_
+
+//Used for messages petitions
+#define CREATE 1
+#define UPDATE 2
+#define READ 3
+#define DELETE 4
+
+//Used for messages responses
+#define HEAD 5
+#define BODY 6
+#define ENDOFCONNECTION 7
+
+
+
+#endif /* MESSAGEACTIONS_H_ */
